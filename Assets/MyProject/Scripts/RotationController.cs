c450c662449a9609ffa7061e9991568197bbdc52
lipindim <lipindim@gmail.com>
2020-10-01 17:33:26 +1000

    Домашняя работа 4

---
+++ Assets/MyProject/Scripts/RotationController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class RotationController : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 5;
+
+    public void Update()
+    {
+        transform.Rotate(transform.forward, Time.deltaTime * _rotationSpeed);
+    }
+}
